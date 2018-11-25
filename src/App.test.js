c785f4ae828e71c6cd7007773b42784e3c44ccb8
was.li <wasuwat.se@gmail.com>
2018-11-25 21:12:19 +0700

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('Increment', () => {
+  const wrapper = shallow(<App />)
+  const instance = wrapper.instance()
+
+  it('should return default value of the count state', () => {
+    expect(wrapper.state().count).toBe(0)
+  })
+  it('should increment count state', () => {
+    instance.increment()
+    expect(wrapper.state().count).toBe(1)
+  })
+  it('should correctly renders count state', () => {
+    expect(wrapper.find('#count').text()).toEqual('1')
+  })
+})
+
+describe('Decrement', () => {
+  const wrapper = shallow(<App />)
+  const instance = wrapper.instance()
+
+  it('should return default value of the count state', () => {
+    expect(wrapper.state().count).toBe(0)
+  })
+  it('should decrement count state', () => {
+    instance.decrement()
+    expect(wrapper.state().count).toBe(-1)
+  })
+  it('should correctly renders count state', () => {
+    expect(wrapper.find('#count').text()).toEqual('-1')
+  })
+})
